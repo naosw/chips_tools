619c7edc2894aa53ef5b1837e8b02355a8278de6
naosw <naoswell@hotmail.com>
2024-02-10 15:32:35 -0500

    Turned test level into text file and added screenshot of it playing

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -1,8 +1,14 @@
 import cc_dat_utils
+import os
 
 #Part 1
+file_path = os.path.dirname(os.path.abspath("pfgd_test.dat"))
 input_dat_file = "data/pfgd_test.dat"
 
+path = "/Users/naosw/OneDrive/Documents/Github/chips_tools/data/pfgd_test.dat"
+# The path provided did not work so I had to manually enter the entire path
 
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
 #print the resulting data
+
+print(cc_dat_utils.make_cc_level_pack_from_dat(path))
